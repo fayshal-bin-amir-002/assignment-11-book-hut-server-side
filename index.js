2391d67afb296ad643fc25331be6d11f42a3ae70
fayshal69 <foyshalbinamir@gmail.com>
2024-05-10 02:02:45 +0600

    added token req

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:5174'
+    ],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 const user = process.env.DB_USER;
 const pass = process.env.DB_PASS;
+const secret_token = process.env.SECRET_ACCESS_TOKEN;
 
 const uri = `mongodb+srv://${user}:${pass}@cluster0.0hiczfr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -28,6 +37,19 @@ async function run() {
 
         const database = client.db("gadgetsDB");
         const productsCollection = database.collection("products");
+
+        //token request
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, secret_token, { expiresIn: "1h" });
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production' ? true : false,
+                    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+                })
+                .send({success: true})
+        })
 
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
